475dc42d282173d8ab8ca4b105ca986caed83782
matilde gallina <matilde.gallina@outlook.com>
2018-08-24 19:00:14 +0200

    grafico sulle statistiche degli interventi di ogni medico

---
+++ dr-House/wwwroot/j.js
@@ -0,0 +1,188 @@
+function Doctor(name, statistics){
+    this.name = name
+    this.statistics = statistics
+}
+
+var dr1 = new Doctor("Mario", [50, 25, 25])
+var dr2 = new Doctor("Luigi", [75, 20, 5])
+
+console.log("dr1: " + dr1)
+console.log("dr1 name: " + dr1.name)
+console.log("dr1 stat: " + dr1.statistics)
+
+var doctors = [dr1, dr2]
+
+
+console.log("doctors" + doctors)
+
+console.log("inizio for")
+for(let d of doctors){
+    console.log("d: " + d)
+    console.log("d: " + d.name)
+    console.log("d: " + d.statistics)
+}
+console.log("fine for")
+
+var drStatistics = []
+console.log("drStatistics iniziale: " + drStatistics)
+for(var dr of doctors){
+    console.log("dr: " + dr)
+    console.log("statistiche dr: " + dr.statistics)
+    drStatistics.push(dr.statistics)
+    console.log("drStatistics: " + drStatistics)
+}
+
+
+
+
+
+
+
+
+
+
+
+var myCanvas = document.getElementById("myCanvas");
+myCanvas.width = 300;
+myCanvas.height = 300;
+  
+var ctx = myCanvas.getContext("2d");
+
+function drawLine(ctx, startX, startY, endX, endY, color){
+    ctx.save();
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(startX,startY);
+    ctx.lineTo(endX,endY);
+    ctx.stroke();
+    ctx.restore();
+}
+
+function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height,color){
+    ctx.save();
+    ctx.fillStyle=color;
+    ctx.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
+    ctx.restore();
+}
+
+function legendItem(name, color, ul){
+    var li = document.createElement("li");
+    li.style.listStyle = "none";
+    li.style.borderLeft = "20px solid " + color;
+    li.style.padding = "5px";
+    li.textContent = name;
+    ul.append(li);
+}
+
+var Barchart = function(options){
+    this.options = options;
+    this.canvas = options.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.colors = options.colors;
+  
+    this.draw = function(){
+        var maxValue = 100;
+        
+        var canvasActualHeight = this.canvas.height - this.options.padding * 2;
+        var canvasActualWidth = this.canvas.width - this.options.padding * 2;
+ 
+        //drawing the grid lines
+        var gridValue = 0;
+        while (gridValue <= maxValue){
+            var gridY = canvasActualHeight * (1 - gridValue/maxValue) + this.options.padding;
+            
+            drawLine(
+                this.ctx,
+                0,
+                gridY,
+                this.canvas.width,
+                gridY,
+                this.options.gridColor
+            );
+
+            //writing grid markers
+            this.ctx.save();
+            this.ctx.fillStyle = this.options.gridColor;
+            this.ctx.font = "bold 10px Arial";
+            this.ctx.fillText(gridValue, 10,gridY - 2);
+            this.ctx.restore();
+ 
+            gridValue+=this.options.gridScale;
+        }
+  
+        //drawing the bars
+        var collIndex = 0
+        var numberOfBars = Object.keys(this.options.data).length;
+        var barSize = (canvasActualWidth - (5 * this.options.padding))/numberOfBars;
+        
+        console.log("numero di colonne: " + numberOfBars)
+        console.log("option.data: " + this.options.data)
+        
+        for(var dr of this.options.data){
+            
+            console.log("[dr]: " + dr)
+            console.log("option.data[dr]: " + this.options.data[dr])
+
+            var barIndex = 0;
+            var startX = 5 * this.options.padding + collIndex * barSize
+            var startY
+
+            console.log("startX: " + startX)
+            console.log("startY: " + startY)
+
+            for(var p of dr.statistics){
+                console.log("p: " + p)
+                
+                var barHeight = Math.round( canvasActualHeight * p / maxValue) ;
+                console.log("altezza barra: " + barHeight)
+
+                console.log("indice barra: " + barIndex)
+                if(barIndex == 0)
+                    startY = this.canvas.height - barHeight - this.options.padding
+                else{
+                    startY = startY - barHeight
+                }
+
+                console.log("startX: " + startX)
+                console.log("startY: " + startY)
+                
+                drawBar(
+                    this.ctx, // contesto
+                    startX, // coordinata x di partenza
+                    startY, // coordinata y di partenza
+                    barSize - this.options.padding, //width
+                    barHeight, // height
+                    this.colors[barIndex%this.colors.length] //colore barra
+                )
+
+                barIndex++;
+            }
+
+            var labelText = dr.name;
+            this.ctx.fillStyle = "black";
+            this.ctx.font = "bold 20px Arial";
+            this.ctx.fillText(labelText, startX, (this.canvas.height));
+
+            collIndex ++;
+        }
+
+        var legend = document.querySelector("legend[for='myCanvas']");
+        var ul = document.createElement("ul");
+        legend.append(ul);
+        legendItem("% Successi", this.colors[0 % this.colors.length], ul)
+        legendItem("% Complicazioni", this.colors[1 % this.colors.length], ul)
+        legendItem("% Decessi", this.colors[2 % this.colors.length], ul)
+    }
+}
+
+var myBarchart = new Barchart(
+    {
+        canvas: myCanvas,
+        padding: 10,
+        gridScale: 10,
+        gridColor: "#ccc",
+        data: doctors,
+        colors: ["green","yellow", "red"]
+    }
+);
+myBarchart.draw();
