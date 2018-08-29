08e489991e2efed5eb16382f20a303b597e70410
matilde gallina <matilde.gallina@outlook.com>
2018-08-29 19:23:00 +0200

    checkbox grafico materiali

--- dr-House/wwwroot/materials-statistics-graph.js
+++ dr-House/wwwroot/materials-statistics-graph.js
@@ -1,41 +1,67 @@
 var myCanvas = document.getElementById("materialsCanvas");
 myCanvas.width = 300;
 myCanvas.height = 300;
-  
+
+var successCB = document.getElementById("succ");
+var complicationsCB = document.getElementById("complic");
+var deathCB = document.getElementById("dec");
+
+var filter = ["successo", "complicazioni", "decesso"]
+
 var ctx = myCanvas.getContext("2d");
 
-function drawLine(ctx, startX, startY, endX, endY, color){
+function drawLine(ctx, startX, startY, endX, endY, color) {
     ctx.save();
     ctx.strokeStyle = color;
     ctx.beginPath();
-    ctx.moveTo(startX,startY);
-    ctx.lineTo(endX,endY);
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(endX, endY);
     ctx.stroke();
     ctx.restore();
 }
 
 // drawLine(ctx, 0, 0, 0, 100, `#ccc`);
 
-function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height, color){
+function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height, color) {
     ctx.save();
-    ctx.fillStyle=color;
-    ctx.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
+    ctx.fillStyle = color;
+    ctx.fillRect(upperLeftCornerX, upperLeftCornerY, width, height);
     ctx.restore();
 }
 
-var Barchart = function(options){
+var div = document.querySelector("div[for='materialsCanvas']");
+div.height = myCanvas.height
+div.width = myCanvas.width
+
+var h2 = document.createElement("h2");
+h2.style.padding = "5px";
+h2.textContent = "Materiali:";
+div.append(h2);
+
+var ul = document.createElement("ul");
+div.append(ul);
+
+for (let categ in oper[0].materials) {
+    var li = document.createElement("li");
+    li.style.listStyle = "none";
+    li.style.padding = "5px";
+    li.textContent = categ;
+    ul.append(li);
+}
+
+var Barchart = function (options) {
     this.options = options;
     this.options.padding = 20;
     this.canvas = options.canvas;
     this.ctx = this.canvas.getContext("2d");
     this.colors = options.colors;
-  
-    this.draw = function(){
+
+    this.draw = function () {
         var maxValue = 0;
         var nCateg = 0
 
         console.log("numero categorie: " + nCateg)
-        for(var c in this.options.data[0].materials){
+        for (var c in this.options.data[0].materials) {
             nCateg++
         }
         console.log("numero categorie: " + nCateg)
@@ -43,7 +69,7 @@ var Barchart = function(options){
         console.log("numberOfCateg: " + numberOfCateg)
 
         var finalData = []
-        while(nCateg > 0){
+        while (nCateg > 0) {
             finalData.push(0)
             nCateg--
         }
@@ -51,17 +77,50 @@ var Barchart = function(options){
         var barIndex = 0
         console.log("lunghezza array: " + finalData.length)
 
-        for(var op of this.options.data){
-            while(barIndex < finalData.length){
-                for(var c in op.materials){
-                    finalData[barIndex] += op.materials[c]
-                    barIndex++
+        for (var op of this.options.data) {
+            if (filter.includes(op.result)) {
+                while (barIndex < finalData.length) {
+                    console.log("esito: " + op.result)
+                    for (let e of filter) {
+                        console.log("e f: " + e)
+                    }
+                    console.log("presente? " + filter.includes(op.result))
+
+
+                    for (var c in op.materials) {
+                        finalData[barIndex] += op.materials[c]
+                        barIndex++
+                    }
+
+
+                    // if(op.result === filter[0]){
+                    //     for(var c in op.materials){
+                    //         finalData[barIndex] += op.materials[c]
+                    //         barIndex++
+                    //     }
+                    // }
+                    // else if(op.result === filter[0]){
+                    //     for(var c in op.materials){
+                    //         finalData[barIndex] += op.materials[c]
+                    //         barIndex++
+                    //     }
+                    // }
+                    // else if(op.result === filter[0]){
+                    //     for(var c in op.materials){
+                    //         finalData[barIndex] += op.materials[c]
+                    //         barIndex++
+                    //     }
+                    // }
+                    // for(var c in op.materials){
+                    //     finalData[barIndex] += op.materials[c]
+                    //     barIndex++
+                    // }
                 }
             }
             barIndex = 0
         }
 
-        for(var i of finalData){
+        for (var i of finalData) {
             console.log(i)
             maxValue = Math.max(maxValue, i);
         }
@@ -69,12 +128,12 @@ var Barchart = function(options){
 
         var canvasActualHeight = this.canvas.height - this.options.padding * 2;
         var canvasActualWidth = this.canvas.width - this.options.padding * 2;
- 
+
         //drawing the grid lines
         var gridValue = 0;
-        while (gridValue <= maxValue){
-            var gridX = canvasActualWidth * (1 - gridValue/maxValue) + this.options.padding;
-            
+        while (gridValue <= maxValue) {
+            var gridX = canvasActualWidth * (1 - gridValue / maxValue) + this.options.padding;
+
             drawLine(
                 this.ctx,
                 this.canvas.width - gridX,
@@ -91,14 +150,41 @@ var Barchart = function(options){
             this.ctx.fillText(gridValue, this.canvas.width - gridX + 2, 10);
             this.ctx.restore();
 
-            gridValue+=this.options.gridScale;
+            gridValue += this.options.gridScale;
         }
-  
+
+        if (filter.length === 0) {
+            console.log("pippo")
+            maxValue = 50
+            while (gridValue <= maxValue) {
+                console.log("pluto")
+                var gridX = canvasActualWidth * (1 - gridValue / maxValue) + this.options.padding
+
+                drawLine(
+                    this.ctx,
+                    this.canvas.width - gridX,
+                    0,
+                    this.canvas.width - gridX,
+                    this.canvas.height - this.options.padding,
+                    this.options.gridColor
+                );
+
+                //writing grid markers
+                this.ctx.save();
+                this.ctx.fillStyle = this.options.gridColor;
+                this.ctx.font = "bold 10px Arial";
+                this.ctx.fillText(gridValue, this.canvas.width - gridX + 2, 10);
+                this.ctx.restore();
+
+                gridValue += this.options.gridScale;
+            }
+        }
+
         //drawing the bars
         // var numberOfBars = Object.keys(this.options.data).length;
         var numberOfBars = numberOfCateg
-        var barSize = (canvasActualHeight)/numberOfBars;
-        
+        var barSize = (canvasActualHeight) / numberOfBars;
+
         // while(barIndex < this.options.data[0].length){
         //     var val = 0
         //     for(var op of this.options.data){
@@ -109,38 +195,38 @@ var Barchart = function(options){
         // }
         // barIndex = 0
 
-        for (var categ of finalData){
+        for (var categ of finalData) {
             var val = categ;
-            var barHeight = Math.round( canvasActualWidth * val/maxValue) ;
+            var barHeight = Math.round(canvasActualWidth * val / maxValue);
             drawBar(
                 this.ctx,
                 this.options.padding,
                 this.options.padding + barIndex * barSize,
                 barHeight,
                 barSize - (this.options.padding / 2),
-                this.colors[barIndex%this.colors.length]
+                this.colors[barIndex % this.colors.length]
             );
 
             // var labelText = "!!";
             // this.ctx.fillStyle = "black";
             // this.ctx.font = "bold 20px Arial";
             // this.ctx.fillText(labelText, (this.canvas.width) -  100, 10);
- 
+
             barIndex++;
         }
 
-        
-        /*********************************************** */
-        
-        var div = document.querySelector("div[for='materialsCanvas']");
-        div.height = myCanvas.height
-        div.width = myCanvas.width
 
-        var h2 = document.createElement("h2");
-        h2.style.padding = "5px";
-        h2.textContent = this.options.seriesName;
-        div.append(h2);
-         
+        /*********************************************** */
+
+        // var div = document.querySelector("div[for='materialsCanvas']");
+        // div.height = myCanvas.height
+        // div.width = myCanvas.width
+
+        // var h2 = document.createElement("h2");
+        // h2.style.padding = "5px";
+        // h2.textContent = this.options.seriesName;
+        // div.append(h2);
+
         // //drawing series name
         // this.ctx.save();
         // this.ctx.textBaseline="bottom";
@@ -150,18 +236,18 @@ var Barchart = function(options){
         // this.ctx.fillText(this.options.seriesName, this.canvas.width / 2, this.canvas.height);
         // this.ctx.restore();
 
-        var ul = document.createElement("ul");
-        div.append(ul);
+        // var ul = document.createElement("ul");
+        // div.append(ul);
 
-        for (let categ in this.options.data[0].materials){
-            var li = document.createElement("li");
-            li.style.listStyle = "none";
-            li.style.padding = "5px";
-            li.textContent = categ;
-            ul.append(li);
-        }
+        // for (let categ in this.options.data[0].materials){
+        //     var li = document.createElement("li");
+        //     li.style.listStyle = "none";
+        //     li.style.padding = "5px";
+        //     li.textContent = categ;
+        //     ul.append(li);
+        // }
         /*********************************************** */
-         
+
         // //draw legend
         // barIndex = 0;
         // var legend = document.querySelector("legend[for='materialsCanvas']");
@@ -180,16 +266,6 @@ var Barchart = function(options){
     }
 }
 
-
-
-var myVinyls = {
-    "Classical music": 10,
-    "Alternative rock": 14,
-    "Pop": 2,
-    "Jazz": 12,
-    "Commercial": 20
-};
-
 var myBarchart = new Barchart(
     {
         canvas: myCanvas,
@@ -197,7 +273,81 @@ var myBarchart = new Barchart(
         gridColor: "#ccc",
         seriesName: "Materials",
         data: oper,
-        colors: ["#a55ca5","#67b6c7", "#bccd7a","#eb9743", "#a55cf6"]
+        colors: ["#a55ca5", "#67b6c7", "#bccd7a", "#eb9743", "#a55cf6"]
     }
 );
 myBarchart.draw();
+
+
+console.log("successo: " + successCB.checked)
+if (successCB.checked) {
+    console.log("funziona")
+}
+
+
+function succClicked(event) {
+    if (successCB.checked) {
+        filter.splice(0, 0, "successo")
+    }
+    else {
+        filter.splice(0, 1)
+    }
+
+    console.log("elementi in filter:")
+    for (let i of filter) {
+        console.log(i)
+    }
+
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    myBarchart.draw()
+}
+
+function complicClicked(event) {
+    if (filter.includes("successo")) {
+        if (complicationsCB.checked) {
+            filter.splice(1, 0, "complicazioni")
+        }
+        else {
+            filter.splice(1, 1)
+        }
+    }
+    else {
+        if (complicationsCB.checked) {
+            filter.splice(0, 0, "complicazioni")
+        }
+        else {
+            filter.splice(0, 1)
+        }
+    }
+
+    console.log("elementi in filter:")
+    for (let i of filter) {
+        console.log(i)
+    }
+
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    myBarchart.draw()
+
+    for (let i of filter) {
+        console.log(i)
+    }
+}
+
+function decClicked(event) {
+    if (deathCB.checked) {
+        filter.push("decesso")
+    }
+    else {
+        filter.pop()
+    }
+
+    console.log("elementi in filter:")
+    for (let i of filter) {
+        console.log(i)
+    }
+
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    myBarchart.draw()
+}
+
+console.log("fine!")
